bd0e4b9a97605f830a23f45ff802bf7df4cef2db
DmitryDorofeev <dorof21@gmail.com>
2018-09-03 20:30:51 +0300

    add logger

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const http = require('http');
 const fs = require('fs');
+const debug = require('debug');
+
+const log = debug('*');
 
 const server = http.createServer((req, res) => {
   const filename = req.url === '/' ? './index.html' : `.${req.url}.html`;
 
+  log('request: %s', req.url);
+
   fs.readFile(filename, (err, file) => {
     if (err) {
+      log('file %s not found', filename);
+
       res.statusCode = 404;
       res.write('404');
       res.end();
       return;
     }
 
+    log('file %s found', filename);
     res.write(file);
     res.end();
   });
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+  log('server started on port %d', 3000);
+});
