08bc42729c70b5d1d84c624f25c98999b1160bf3
DmitryDorofeev <dorof21@gmail.com>
2018-09-03 20:14:56 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const filename = req.url === '/' ? './index.html' : `.${req.url}.html`;
+
+  fs.readFile(filename, (err, file) => {
+    if (err) {
+      res.statusCode = 404;
+      res.write('404');
+      res.write('404');
+      res.write('404');
+      res.write('404');
+      res.end();
+      return;
+    }
+
+    res.write(file);
+    res.end();
+  });
+});
+
+server.listen(3000);
